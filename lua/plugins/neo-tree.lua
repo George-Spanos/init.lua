010f91004118b0898e3b6293cacdd9c393fed2f5
George Spanos <gespa11019@gmail.com>
2023-07-13 15:11:50 +0300

    change neotree to toggle

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,14 +1,20 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    keys = {
-        { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Open NeoTree" },
-    },
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-    },
-    config = function()
-        require("neo-tree").setup()
-    end,
+	"nvim-neo-tree/neo-tree.nvim",
+	keys = {
+		{ "<leader>e", "<cmd>NeoTreeFloatToggle<cr>", desc = "Open NeoTree" },
+	},
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+				},
+			},
+		})
+	end,
 }
