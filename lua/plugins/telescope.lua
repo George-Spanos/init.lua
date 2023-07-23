b52703bbe0409fce0c4b4ef9b726a2b35da41da1
George Spanos <gspanos@moby-it.com>
2023-07-23 16:23:39 +0300

    update telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.1",
+	tag = "0.1.2",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	keys = {
 		{ "<leader>sf", "<cmd>Telescope find_files<cr>", mode = { "n" }, desc = "Search Files" },
