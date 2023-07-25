c24a670e7826efddf3de1645a997f384de2a5a96
George Spanos <gespa11019@gmail.com>
2023-07-25 10:04:43 +0300

    (chore) - formatting

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,5 +1,5 @@
-vim.keymap.set("n", "<C-h>", "<cmd>wincmd h<cr>",  { desc = "Go to left window" })
+vim.keymap.set("n", "<C-h>", "<cmd>wincmd h<cr>", { desc = "Go to left window" })
 vim.keymap.set("n", "<C-l>", "<cmd>wincmd l<cr>", { desc = "Go to right window" })
 vim.keymap.set("n", "<C-j>", "<cmd>wincmd j<cr>", { desc = "Go to lower window" })
 vim.keymap.set("n", "<C-k>", "<cmd>wincmd k<cr>", { desc = "Go to upper window" })
-vim.keymap.set("n","<F3>","<cmd>:noh<cr>",{ desc = "Clear Search Highlight"})
+vim.keymap.set("n", "<F3>", "<cmd>:noh<cr>", { desc = "Clear Search Highlight" })
